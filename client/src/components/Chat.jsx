03cd18d4ba6582d54f7f444d1450387b0d6ca368
User <ubitoz133@gmail.com>
2026-01-18 02:37:35 +0400

    dev slop

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -10,7 +10,7 @@ export default function Chat() {
   const navigate = useNavigate();
 
   socket.on("invalid-username", () => {
-    socket.disconnect();
+    socket.disconnect;
     navigate("/");
   });
 
