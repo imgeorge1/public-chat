c54fcbd55ebc22ab7a0d1e2f4543fe3b50ca005d
Giorgi <127306696+imgeorge1@users.noreply.github.com>
2026-03-04 22:29:25 +0400

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 // socket.js
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000", {
+export const socket = io("https://public-chat2.onrender.com", {
   transports: ["websocket"],
 });
 
