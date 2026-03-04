7c5cc8f31bde620445b5cccdc575ecf7cb4433ed
imgeorge1 <ubitoz133@gmail.com>
2026-03-04 21:22:05 +0400

    changed cors origin

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -18,7 +18,9 @@ export default function Chat() {
   async function getMessages() {
     // change in production https://public-chat2.onrender.com
     try {
-      const response = await axios.get("http://localhost:3000/message/");
+      const response = await axios.get(
+        "https://public-chat2.onrender.com/message/",
+      );
       // console.log(response.data);
       setMessageArr(response.data.rows);
     } catch (error) {
--- client/src/components/MessageItem.jsx
+++ client/src/components/MessageItem.jsx
@@ -10,7 +10,9 @@ export default function Item({
   async function messageDelete(msgID) {
     console.log(msgID);
     try {
-      await axios.delete(`http://localhost:3000/messageDelete/${msgID}`); //need to change later
+      await axios.delete(
+        `https://public-chat2.onrender.com/messageDelete/${msgID}`,
+      ); //need to change later
     } catch (error) {
       console.log(error);
     }
